20647d15fe5a5bfe126698d964d71b6d5b3d881a
Jarian Martinez <jarianm2@nycstudents.net>
2024-12-04 13:59:25 +0000

    fruit

--- U5T7/Main7.java
+++ U5T7/Main7.java
@@ -1,13 +1,8 @@
 public class Main7 {
+    public static double cArea(double radius) {
+        return (Math.pow(radius, 2) * 3.1415);
+    }
     public static void main(String[] args) {
-        System.out.println(RaceUtility.milesToKm(13.85));
-        System.out.println(RaceUtility.milesToKm(26.42));
-        System.out.println(RaceUtility.kmToMiles(40));
-        System.out.println(RaceUtility.kmToMiles(3.5));
-        String title = "welcome to the marathon!";
-        System.out.println(RaceUtility.makeProper(title));
-        String title2 = "It's TIME for THE 5k!";
-        System.out.println(RaceUtility.makeProper(title2));
-
+        System.out.println(cArea(2));
     }
 }
