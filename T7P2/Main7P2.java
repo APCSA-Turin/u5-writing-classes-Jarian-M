c2f87843bcbd83f51ba51bbae558b90d0e7a3ac3
Jarian Martinez <jarianm2@nycstudents.net>
2024-12-04 14:20:07 +0000

    kiwi

--- T7P2/Main7P2.java
+++
@@ -1,9 +0,0 @@
-package T7P2;
-public class Main7P2 {
-    public static void main(String[] args) {
-        System.out.println(cArea(2));
-    }
-    public static double cArea(double radius) {
-        return (Math.pow(radius, 2) * 3.1415);
-    }
-}
--- U5T7/Main7.java
+++ U5T7/Main7.java
@@ -1,8 +1,8 @@
 public class Main7 {
     public static double cArea(double radius) {
-        return (Math.pow(radius, 2) * 3.1415);
+        return (Math.pow(radius,2) * 3.1415);
     }
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         System.out.println(cArea(2));
     }
 }
